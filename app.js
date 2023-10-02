bc3ff9cbf7919d204529bdcd440b61f477657237
muhammadYousof <my 7370044@gmail.com>
2023-10-02 13:06:51 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,141 @@
+
+        // --------- adiction/Subtraction --------
+
+        document.write("<h1>Value After Variable Declaration is Undefined</h1")
+
+
+    var num1 = 20;
+    
+    var num2 = 20;
+    
+    var sum = num1 + num2;
+
+    document.write("</br>","Addition of 20 of 20","=",sum)
+    
+    var num3 = 5;
+    
+    var sum1 = sum - num3
+
+    document.write("</br>","Subtraction of 5 in 40","=",sum1)
+    
+    var num4 = 3;
+    
+    var sum2 = sum1 * num4
+
+
+    document.write("</br>","multiply of 3 to 35","=",sum2)
+    
+    var num5 = 2;
+    
+    var sum3 = sum2 / num5;
+
+
+    document.write("</br>","Divided of 105 to 2","=",sum3)
+    
+    
+    // ------- ticket price ------
+    
+    var ticketprice = 600
+    
+    var getticket = prompt(" Cost of one movie ticket is 600 PKR.")
+    
+    // alert(ticketprice * getticket)
+    document.write("<h1>ticket price</h1>")
+    document.write("</br>","Total Cost to buy",getticket," ticket to a movie is")
+    document.write(ticketprice * getticket)
+    
+
+        // --------- table --------
+
+    var tablenum = 4
+
+    document.write("<h1>Table Of 4</h1>")
+    for(var i = 1; i <= 10; i++)
+        document.write("</br>",tablenum +" "+ "*" +" "+ i +" "+ "=" +" "+ i * tablenum)
+
+
+            // ----------- shoping card --------
+
+     document.write("<h1>Shoping Card</h1>") 
+     
+     document.write("Price of Item Is 650")
+     document.write("</br>","price of item 2 is 100")
+     document.write("</br>","Quantity of item 1 is 3")
+     document.write("</br>","Quantity of item 2 is 7")
+     document.write("</br>","Shipping charges is 100")
+
+
+
+     document.write("</br>","Total Cost of your Order is 2750")
+
+            // --------- Mark Sheet ------------
+
+var total = 980
+var obtain = 804
+
+var multiply = 804 * 100
+var ruselt = multiply / total
+
+document.write("<h1>Mark Sheet</h1>")
+
+
+document.write("total number =",total,"</br>")
+document.write("obtain mark =",obtain,"</br>")
+document.write("my persentage =",ruselt,"%","</br>")
+
+
+// ------ currency PKR ----------
+
+document.write("<h1>currency PKR</h1>")
+
+var exchangeRates1 = " 1 US Dollar = 104.80 Pakistani Rupee "
+var exchangeRates2 = " 1 Saudi Riyal = 28 Pakistani Rupee"
+
+console.log(exchangeRates1 +  exchangeRates2)
+
+document.write("total Currency in PKR: 1748")
+
+                // --------- The Geometrizer --------
+
+document.write("<h1>The Geometrizer</h1>")
+
+    var radius = 20
+    var circumfarance = 2*3.14*radius
+
+    console.log(circumfarance)
+
+    document.write("Radius of a circle :20")
+    document.write("<br>", "Circumference :" , circumfarance)
+
+    var area = 3.14*radius*radius
+    
+    document.write( "<br>","The Area is", area)
+
+
+        // --------- Age colculetor ---------
+
+   document.write("<h1>Age colculetor</h1>")
+
+document.write("</br>", "current year 2023") 
+document.write("</br>", "birth year 2002") 
+
+
+document.write("</br>","your Age is: 21")
+
+
+// --------- The Lifetime Supply Calculetor ---------
+
+
+document.write("<h1>The Lifetime Supply Calculetor</h1>")
+
+document.write("</br>","Favourite Snack: Chocolate Chip")
+document.write("</br>","Current Age:21")
+document.write("</br>","Estimated Maximum age: 65")
+document.write("</br>","Amount of Snack Per Day: 3")
+document.write("</br>","You Will Need 150 Chocolate Chip to Last You Until The Ripe Old Age Of 65")
+
+
+
+
+
+
